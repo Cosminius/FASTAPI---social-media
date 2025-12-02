a3e90c72ede94ab02c1021fdf7d3727dade08367
cosmin <rosculet.cosmin@yahoo.com>
2025-12-02 14:18:39 +0200

    aaa

--- app/app.py
+++ app/app.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.schemas import PostCreate
 
 app = FastAPI()
 
-text_posts={"post1 ":{"title" : "First Post", "content": "This is the content of the first post"},
-            "post2 ":{"title" : "Second Post", "content": "This is the content of the second post"},
-            "post3 ":{"title" : "Third Post", "content": "This is the content of the third post"}}
+text_posts = {
+    1: {"title": "First Post", "content": "This is the content of the first post"},
+    2: {"title": "Second Post", "content": "This is the content of the second post"},
+    3: {"title": "Third Post", "content": "This is the content of the third post"},
+    4: {"title": "Fourth Post", "content": "This is the content of the fourth post"},
+    5: {"title": "Fifth Post", "content": "This is the content of the fifth post"},
+    6: {"title": "Sixth Post", "content": "This is the content of the sixth post"},
+    7: {"title": "Seventh Post", "content": "This is the content of the seventh post"},
+    8: {"title": "Eighth Post", "content": "This is the content of the eighth post"},
+    9: {"title": "Ninth Post", "content": "This is the content of the ninth post"},
+    10: {"title": "Tenth Post", "content": "This is the content of the tenth post"},
+}
 
 
 @app.get('/api/root')
@@ -12,10 +22,27 @@ def root():
     return {"Message": "Welcome to my API"}    
 
 @app.get("/api/posts")
-def get_allposts():
+def get_all_posts(limit: int = None): #poti sa faci sa fie obligatoriu 
+    if limit:
+        return list(text_posts.values())[:limit]
     return text_posts
 
 @app.get("/api/posts/{post_id}")
-def get_post(post_id: str):
-    post_id = post_id.strip().lower()
+def get_post(post_id: int):
+    if post_id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
     return text_posts.get(post_id)
+
+
+@app.post("/api/posts")
+def create_post(post: PostCreate):
+    post_id = max(text_posts.keys()) + 1
+    text_posts[post_id] = post.model_dump()
+    return text_posts[post_id]
+
+@app.delte("/api/posts/{post_id}")
+def delete_post(post_id: int):
+    if post_id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
+    text.post
+
