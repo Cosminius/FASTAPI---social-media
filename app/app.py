2e47d176ffb56133b04dae82778b91b9231c2b97
cosmin <rosculet.cosmin@yahoo.com>
2025-12-03 15:52:55 +0200

    aaa

--- app/app.py
+++ app/app.py
@@ -1,48 +1,50 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Depends, Form
+from sqlalchemy import select
 from app.schemas import PostCreate
+from app.db import Post, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
-
-text_posts = {
-    1: {"title": "First Post", "content": "This is the content of the first post"},
-    2: {"title": "Second Post", "content": "This is the content of the second post"},
-    3: {"title": "Third Post", "content": "This is the content of the third post"},
-    4: {"title": "Fourth Post", "content": "This is the content of the fourth post"},
-    5: {"title": "Fifth Post", "content": "This is the content of the fifth post"},
-    6: {"title": "Sixth Post", "content": "This is the content of the sixth post"},
-    7: {"title": "Seventh Post", "content": "This is the content of the seventh post"},
-    8: {"title": "Eighth Post", "content": "This is the content of the eighth post"},
-    9: {"title": "Ninth Post", "content": "This is the content of the ninth post"},
-    10: {"title": "Tenth Post", "content": "This is the content of the tenth post"},
-}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
 
 
-@app.get('/api/root')
-def root():
-    return {"Message": "Welcome to my API"}    
+app = FastAPI(lifespan=lifespan)
 
-@app.get("/api/posts")
-def get_all_posts(limit: int = None): #poti sa faci sa fie obligatoriu 
-    if limit:
-        return list(text_posts.values())[:limit]
-    return text_posts
+@app.post("/upload/")
+async def upload_post(
+    file : UploadFile = File(...),
+    caption: str = Form(...),
+    session: AsyncSession = Depends(get_async_session)
+):
+    post = Post(
+        caption=caption,
+        url="dummyurl",
+        file_type="photo",
+        file_name="dummy name"
+    )
+    session.add(post)
+    await session.commit()
+    await session.refresh(post)
+    return post
 
-@app.get("/api/posts/{post_id}")
-def get_post(post_id: int):
-    if post_id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found")
-    return text_posts.get(post_id)
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session. execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.fetchall()]
+    posts_data=[]
+    for post in posts:
+        posts_data.append(
+            {
+                "id": post.id,
+                "caption": post.caption,
+                "url": post.url,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
+    return {"posts": posts_data}
 
-
-@app.post("/api/posts")
-def create_post(post: PostCreate):
-    post_id = max(text_posts.keys()) + 1
-    text_posts[post_id] = post.model_dump()
-    return text_posts[post_id]
-
-@app.delte("/api/posts/{post_id}")
-def delete_post(post_id: int):
-    if post_id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post not found")
-    text.post
 
--- app/db.py
+++ app/db.py
@@ -1,29 +1,31 @@
 from collections.abc import AsyncGenerator
 from datetime import datetime
 import uuid
-from sqlalchemy import Column, String, Text, DateTime, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import Declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship
 
-DATABASE_URL ="sqlite+aiosqlite:///./test.db"
 
-class Post(Declarative_base):
-    __tablename__="posts"
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+
+Base = declarative_base()
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     title = Column(String(200), nullable=False)
-    url = Column(String(200), nullable = False)
+    url = Column(String(200), nullable=False)
     file_name = Column(String(200), nullable=False)
-    created_at = Column(DateTime, default = datetime.now())
+    created_at = Column(DateTime, default=datetime.utcnow)
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(Declarative_base().metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
-async def get_async_session()-> AsyncGenerator[AsyncSession]:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
